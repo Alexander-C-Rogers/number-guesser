a252a8c2238026c276eeb3e84fea56539b94bb02
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-06-20 18:25:05 -0400

    create a compareGuesses() function.

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ let currentRoundNumber = 1;
 const generateTarget = () => {
     return Math.floor(Math.random() * 10);
 };
+
+let humanGuess = 0;
+let computerGuess = 0;
+let targetNumbers = generateTarget();
+
+const compareGuesses = (humanGuess, computerGuess, targetNumbers) => 
+    Math.abs(targetNumbers - humanGuess) <= Math.abs(targetNumbers - computerGuess) ? true : false;
