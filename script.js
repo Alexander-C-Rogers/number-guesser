8e801e2635b186632c026538799bcd4f35327011
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-06-20 18:38:13 -0400

    create a getAbsoluteDistance() function.

--- script.js
+++ script.js
@@ -15,8 +15,16 @@ let humanGuess = 0;
 let computerGuess = 0;
 let targetNumbers = generateTarget();
 
-const compareGuesses = (humanGuess, computerGuess, targetNumbers) => 
-    Math.abs(targetNumbers - humanGuess) <= Math.abs(targetNumbers - computerGuess) ? true : false;
+/*const compareGuesses = (humanGuess, computerGuess, targetNumbers) => 
+    Math.abs(targetNumbers - humanGuess) <= Math.abs(targetNumbers - computerGuess) ? true : false;*/
+
+const getAbsoluteDistance = (guess, target) => Math.abs(target - guess);
+        
+const compareGuesses = (humanGuess, computerGuess, targetNumbers) => getAbsoluteDistance(humanGuess, targetNumbers) <= getAbsoluteDistance(computerGuess, targetNumbers) ? true : false ;
+            
+const check = humanGuess => {
+    if (humanGuess > 9) { window.alert( 'Your number is out of range! Pleas, choose number between 0 and 9.')} 
+}
 
 let winner =  compareGuesses(humanGuess, computerGuess, targetNumbers) ? 'human' : 'computer';
       
