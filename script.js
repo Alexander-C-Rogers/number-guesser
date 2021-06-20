1801a4faebbf3d92d3ce7934e476ba9bdc3ff837
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-06-20 18:21:10 -0400

    create a generateTarget() function.

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ const session = require("express-session");
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
+
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+};
