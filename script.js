fbe7661ec10a11921aa9a36152839e572ef6d57c
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-06-20 13:31:02 -0400

    setting up enviorment.

---
+++ script.js
@@ -0,0 +1,8 @@
+//packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
